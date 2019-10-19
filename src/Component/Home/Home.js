2bbb2f43eecf4ad35d516fae7ee5056d70a78325
Irfan223 <mdirfan844@gmail.com>
2019-10-20 00:03:29 +0530

    chane on home.js

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Home.css';
 import Header from '../Header/Header';
+import { format } from 'path';
+
+
 class Home extends Component {
 
     constructor(props) {
@@ -30,8 +33,36 @@ class Home extends Component {
                 <div className="App">
                     <Header />
                     <section>
-                        <div className="content">
-                            
+                          <div className="content">
+                            {/* <img src={require('../../assets/bg-form.jpg')} /> */}
+
+                            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                                <ol className="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                </ol>
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <img className="d-block w-100" src={require('../../assets/bg-form.jpg')} alt="First slide"></img>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img className="d-block w-100" src={require('../../assets/bg-form2.jpg')} alt="Second slide"></img>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img className="d-block w-100" src={require('../../assets/bg-form2.jpg')} />
+                                    </div>
+                                </div>
+                                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Previous</span>
+                                </a>
+                                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Next</span>
+                                </a>
+                              </div>
+
                             {items.map(item => (
                                 <span key={item.id}>
                                     <p>Title: {item.title}</p>
